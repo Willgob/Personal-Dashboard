674fd5d1a4c684f26b06a33bd3b6b034009f6755
Williamnumismatics <169579672+Williamnumismatics@users.noreply.github.com>
2026-01-23 16:25:05 +1100

    brooooooooooooooooooooo bambu lab still not working sadddd

--- bambulabtest.py
+++ bambulabtest.py
@@ -1,38 +1,14 @@
 import time
-from bambu_connect import BambuClient, PrinterStatus
-from dataclasses import asdict
-import pprint
-from dotenv import load_dotenv
-import os
+from bambu_connect import BambuClient
 
-load_dotenv()
+client = BambuClient("192.168.68.62", "15946490", "01P00C490400556")
 
-# Replace these with your actual details
-hostname = "William"
-access_code = "15946490"
-serial = "01P00C490400556"
+while True:
+    try:
+        info = client.dump_info()
+        print("\n=== POLLED STATUS ===")
+        print(info)
+    except Exception as e:
+        print("Failed:", e)
 
-
-def custom_callback(msg: PrinterStatus):
-    printer_status_dict = asdict(msg)
-    pprint.pprint(printer_status_dict)
-
-
-def on_watch_client_connect():
-    print("WatchClient connected, Waiting for connection...")
-    time.sleep(1)  # Waits for 1 second
-    print("Executing dump_info.")
-    bambu_client.dump_info()
-
-
-bambu_client = BambuClient(hostname, access_code, serial)
-bambu_client.start_watch_client(custom_callback, on_watch_client_connect)
-
-try:
-    while True:
-        time.sleep(1)  # Just keep the main thread alive
-except KeyboardInterrupt:
-    print("Streaming stopped by user.")
-
-# Stop the client
-bambu_client.stop_watch_client()
+    time.sleep(5)
