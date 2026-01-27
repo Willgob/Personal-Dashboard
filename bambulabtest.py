0451f2b94c88027930c589dc8acb89ecdc83989f
Williamnumismatics <169579672+Williamnumismatics@users.noreply.github.com>
2026-01-27 13:17:23 +1100

    trying to make the bambyu thing workl trying to reverse enginner printmate by @0x62b

---
+++ bambulabtest.py
@@ -0,0 +1,35 @@
+import paho.mqtt.client as mqtt
+import ssl
+import json
+import time, random, string
+
+printer_ip = "192.168.68.62"
+serial = "01P00C490400556"
+lan_code = "16dade9c"
+
+client_id = "pdash_" + str(int(time.time()))
+
+def on_connect(client, userdata, flags, rc):
+    print("Connect returned code:", rc)
+    if rc == 0:
+        print("Connected successfully")
+        client.subscribe(f"device/{serial}/report")
+    elif rc == 5:
+        print("❌ Not authorized — wrong LAN code")
+    else:
+        print("❌ Connection failed:", rc)
+
+def on_message(client, userdata, msg):
+    print(msg.topic, msg.payload)
+
+client = mqtt.Client(client_id=client_id, clean_session=True)
+client.username_pw_set(serial, lan_code)
+client.tls_set(cert_reqs=ssl.CERT_NONE)
+client.tls_insecure_set(True)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+print("Connecting...")
+client.connect(printer_ip, 8883, 60)
+client.loop_forever()
