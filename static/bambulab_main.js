67b1b4ea54fa4774af4c96aada0b1013582b8ef3
Williamnumismatics <169579672+Williamnumismatics@users.noreply.github.com>
2026-01-30 11:53:15 +1100

    added basic nozzle temp viewing im gonna make it so when u click it u can input target temp now i might just put all the commands in first tho

---
+++ static/bambulab_main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    async function bambulabpage() {
+        const res = await fetch("/Bambulab/status")
+        const data = await res.json()
+        console.log("loaded");
+
+        bambulab_main_page_data = document.getElementById("bambulab_main_page_nozzle")
+
+        let nozzle_temp = data.print.nozzle_temper;
+        let nozzle_target_temp = data.print.nozzle_target_temper;
+
+        bambulab_main_page_data.innerHTML = `<span class="nozzle_temp_bambulab_page">Nozzle Temp : </span>${Math.round(nozzle_temp)}/${Math.round(nozzle_target_temp)} °C`;
+    }
+
+    bambulabpage();
+});
