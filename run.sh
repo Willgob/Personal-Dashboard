9657f860953f8fb1e79337e108d2b097c8b3676b
Williamnumismatics <169579672+Williamnumismatics@users.noreply.github.com>
2026-02-07 09:53:26 +1100

    added linux run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+echo "Running Personal Dashboard (Linux) ..."
+
+if [ -d "venv" ]; then
+    source venv/bin/activate
+else
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements_linux.txt   
+fi
